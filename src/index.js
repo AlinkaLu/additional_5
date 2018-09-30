e2dd42730863b7ce1e0e0c66e82b8df5b1bee19e
Alina Lutsevich <alinkaluya@gmail.com>
2018-09-30 16:07:50 +0300

    Function half implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ var parentheses = "[](){}",
+  array = [],
+  i, character, bracketsConfig;
+
+for(i = 0; character = str[i]; i++) {
+  bracketsConfig = parentheses.indexOf(character);
+
+  if(bracketsConfig === -1) {
+    continue;
+  }
+
+  if(bracketsConfig% 2 === 0) {
+    array.push(bracketsConfig + 1); 
+  } else {
+    if(array.length === 0 || array.pop() !== bracketsConfig) {
+      return false;
+    }
+  }
 }
+
+return array.length === 0;
+};
